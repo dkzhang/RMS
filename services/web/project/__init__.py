01e3a2cd55af03916dd05b0e2db1c00d21771363
dkzhang@Office.PC <527656264@qq.com>
2020-05-13 15:22:49 +0800

    add postgres db.

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 import base64
 
 app = Flask(__name__)
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
 
 @app.route('/')
 def hello_world():
@@ -11,7 +25,7 @@ def hello_world():
 
 @app.route('/hello')
 def hello():
-    lInt = [0xff,0b11000000]
+    lInt = [0xff, 0b11000000]
     myBytes = bytearray(lInt)
     Base64Str = base64.b64encode(myBytes).decode('ascii')
     return Base64Str
